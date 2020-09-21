6ee4653da17558767c4728e675f804429746997c
Kamal Mahyuddin <kamal@envoy.com>
2020-09-21 11:14:36 -0700

    Remove brocolli-react

--- index.js
+++ index.js
@@ -1,22 +1,6 @@
 /* eslint-env node */
 'use strict';
 
-const react = require('broccoli-react');
-
 module.exports = {
   name: 'ember-cli-react',
-
-  preprocessTree: function(type, tree) {
-    if (type === 'js') {
-      tree = react(tree, { transform: { es6module: true } });
-    }
-
-    return tree;
-  },
-
-  options: {
-    autoImport: {
-      devtool: 'inline-source-map',
-    },
-  },
 };
